50da58722ca4c1d3a13cceed6f0bf2470a9cad93
yedai13 <facundo.marin2012@gmail.com>
2021-09-26 17:24:16 -0300

    agregando test controller

--- src/test/java/ar/edu/unlam/tallerweb1/controladores/TurnoControllerTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/controladores/TurnoControllerTest.java
@@ -1,28 +1,59 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
+import ar.edu.unlam.tallerweb1.common.Frecuencia;
+import ar.edu.unlam.tallerweb1.common.Modalidad;
+import ar.edu.unlam.tallerweb1.common.Tipo;
+import ar.edu.unlam.tallerweb1.modelo.*;
 import ar.edu.unlam.tallerweb1.servicios.ClaseService;
+import ar.edu.unlam.tallerweb1.servicios.TurnoService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 import static org.mockito.Mockito.mock;
 
 public class TurnoControllerTest {
 
     ClaseService claseService = mock(ClaseService.class);
-    TurnoController turnoController = new TurnoController(claseService);
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
+    TurnoService turnoService = mock(TurnoService.class);
+    TurnoController turnoController = new TurnoController(claseService, turnoService );
 
     @Test
-    void test() {
+    public void testQueSePuedaReservarTurno() throws Exception {
+        Clase clase = givenQueLaClaseTengaLugar();
+        Cliente cliente = givenUnClienteActivo();
+        whenReservoTurno(clase);
 
+    }
 
+    private Cliente givenUnClienteActivo() {
+        return new Cliente("Arturo" + LocalDateTime.now(),
+                "Frondizi", "arturitoElMasCapo@gmail.com");
+
+    }
+
+    private Clase givenQueLaClaseTengaLugar() throws Exception {
+        Actividad actividad = givenUnaActividadConPeriodoYHorarioValido();
+        return new Clase(LocalDateTime.now(), actividad, Modalidad.PRESENCIAL);
+    }
+
+    private Actividad givenUnaActividadConPeriodoYHorarioValido() throws Exception {
+        LocalDateTime antesDeAyer = LocalDateTime.now().minusDays(2);
+        LocalDateTime pasadoManiana = LocalDateTime.now().plusDays(2);
+        Periodo periodo = new Periodo(antesDeAyer, pasadoManiana);
+
+        LocalTime ahora = LocalTime.now();
+        LocalTime enUnRato = LocalTime.now().plusHours(2);
+        Horario horario = new Horario(ahora, enUnRato);
+
+        return new Actividad("Actividad de alto impacto", Tipo.CROSSFIT, 4000f, Frecuencia.CON_INICIO_Y_FIN, periodo, horario);
+    }
+
+    private void whenReservoTurno(Clase clase) {
+        turnoController.reservarTurno(clase.getId());
 
     }
 }
