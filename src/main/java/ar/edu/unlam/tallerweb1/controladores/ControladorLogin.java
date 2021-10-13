13524dc268dd32ecf1967fecec6c8bbf67ace3a0
yedai13 <facundo.marin2012@gmail.com>
2021-10-13 00:50:46 -0300

    Update ControladorLogin.java

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorLogin.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorLogin.java
@@ -64,7 +64,7 @@ public class ControladorLogin {
 
 	// Escucha la URL /home por GET, y redirige a una vista.
 	@RequestMapping(path = "/home", method = RequestMethod.GET)
-	public ModelAndView irAHome() {return new ModelAndView("home");}
+	public ModelAndView irAHome() {return new ModelAndView("redirect:/mostrar-clases");}
 
 	// Escucha la url /, y redirige a la URL /login, es lo mismo que si se invoca la url /login directamente.
 	@RequestMapping(path = "/", method = RequestMethod.GET)
