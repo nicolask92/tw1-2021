5b335d5b58d36606104a743d9fef83c52dde133d
yedai13 <facundo.marin2012@gmail.com>
2021-11-11 11:08:10 -0300

    agregando clases para mes noviembre, y usuarios con plan standard y premium

--- sql.sql
+++ sql.sql
@@ -1,10 +1,7 @@
-
-insert into usuario (DTYPE, activo, apellido, email, nombre, password, rol, plan)
-values ("Cliente", true, "Cliente", "cliente@gmail.com", "Jorge", "cliente123", "Rol de cliente", "BASICO");
-
 insert into calendario(id)
 values    (1 );
 
+/*-----------HORARIOS-----------------------*/
 insert into horario (id, hora_fin, hora_inicio)
 values (1, "10:30:20", "12:30:20");
 
@@ -17,11 +14,15 @@ values (3, "14:30:20", "16:30:20");
 insert into horario (id, hora_fin, hora_inicio)
 values (4, "14:30:20", "16:30:20");
 
+/*-----------ACTIVIDADES-------------*/
+
 insert into actividad (id, descripcion, precio, tipo, calendario_id)
 values (1, "CrossFit alto impacto", 2000, "CROSSFIT", 1);
 
 insert into actividad (id, descripcion, precio, tipo, calendario_id)
 values (2, "Aquagym", 4000, "NATACION", 1);
+
+/*----------CLASES OCTUBRE-----------*/
 
 insert into clase (cupoMaximo, diaClase, modalidad, actividad_id, horario_id)
 values(20        , "2021-10-03T10:15:30", "PRESENCIAL", 1, 1);
@@ -36,6 +37,9 @@ insert into clase (cupoMaximo, diaClase, modalidad, actividad_id, horario_id)
 values(20        , "2021-10-20T10:15:30", "PRESENCIAL", 1, 4);
 
 insert into clase (cupoMaximo, diaClase, modalidad, actividad_id, horario_id)
+values(20        , "2021-10-20T14:15:30", "PRESENCIAL", 2, 3);
+
+insert into clase (cupoMaximo, diaClase, modalidad, actividad_id, horario_id)
 values(20        , "2021-10-27T23:15:30", "PRESENCIAL", 1, 4);
 
 insert into clase (cupoMaximo, diaClase, modalidad, actividad_id, horario_id)
@@ -43,6 +47,34 @@ values(20        , "2021-10-29T02:15:30", "PRESENCIAL", 1, 4);
 
 insert into clase (cupoMaximo, diaClase, modalidad, actividad_id, horario_id)
 values(20        , "2021-10-30T10:15:30", "PRESENCIAL", 1, 4);
+
+/*----------CLASES NOVIEMBRE-----------*/
+
+insert into clase (cupoMaximo, diaClase, modalidad, actividad_id, horario_id)
+values(20        , "2021-11-03T10:15:30", "PRESENCIAL", 1, 1);
+
+insert into clase (cupoMaximo, diaClase, modalidad, actividad_id, horario_id)
+values(20        , "2021-11-06T10:15:30", "PRESENCIAL", 2, 2);
+
+insert into clase (cupoMaximo, diaClase, modalidad, actividad_id, horario_id)
+values(20        , "2021-11-10T10:15:30", "PRESENCIAL", 1, 3);
+
+insert into clase (cupoMaximo, diaClase, modalidad, actividad_id, horario_id)
+values(20        , "2021-11-20T10:15:30", "PRESENCIAL", 1, 4);
+
+insert into clase (cupoMaximo, diaClase, modalidad, actividad_id, horario_id)
+values(20        , "2021-11-20T14:15:30", "PRESENCIAL", 2, 3);
+
+insert into clase (cupoMaximo, diaClase, modalidad, actividad_id, horario_id)
+values(20        , "2021-11-27T23:15:30", "PRESENCIAL", 1, 4);
+
+insert into clase (cupoMaximo, diaClase, modalidad, actividad_id, horario_id)
+values(20        , "2021-11-29T13:15:30", "PRESENCIAL", 1, 4);
+
+insert into clase (cupoMaximo, diaClase, modalidad, actividad_id, horario_id)
+values(20        , "2021-11-30T10:15:30", "PRESENCIAL", 1, 4);
+
+/*----------------------------------------------------------------------------------*/
 
 insert into diayhorario (id, dia, horario_id)
 values (1, "Lunes", 1);
@@ -52,6 +84,11 @@ values (1, 1);
 
 INSERT INTO turno(id, asisitio, fecha_y_hora_de_reserva, clase_id, cliente_id) VALUES (1, 'false', '2021-10-03 10:15:30.000000', 4, 1);
 
+/*-------USUARIOS-----------*/
+
+insert into usuario (DTYPE, activo, apellido, email, nombre, password, rol, plan)
+values ("Cliente", true, "Cliente", "cliente@gmail.com", "Jorge", "cliente123", "Rol de cliente", "BASICO");
+
 insert into usuario  (id, DTYPE, activo, apellido, email, nombre, password, rol)
 values (2, "Entrenador", true, "Entrenador 1", "entranador@gmail.com", "Lopez", "entrenador123", "Rol de entrenador");
 
@@ -59,6 +96,10 @@ insert into usuario  (id, DTYPE, activo, apellido, email, nombre, password, rol)
 values (3, "Entrenador", true, "Entrenador 2", "entranador2@gmail.com", "Luis", "entrenador123", "Rol de entrenador");
 
 insert into usuario(DTYPE, activo, apellido, email, nombre, password, rol, plan)
-values ("Cliente", true, "clienteSinPlan", "cliente2@gmail.com", "Manuel", "cliente123", "Rol de cliente", "NINGUNO")
+values ("Cliente", true, "clienteSinPlan", "cliente2@gmail.com", "Manuel", "cliente123", "Rol de cliente", "NINGUNO");
 
+insert into usuario(DTYPE, activo, apellido, email, nombre, password, rol, plan)
+values ("Cliente", true, "clientePlanEstandar", "clienteestandar@gmail.com", "Pedro", "cliente123", "Rol de cliente", "ESTANDAR");
 
+insert into usuario(DTYPE, activo, apellido, email, nombre, password, rol, plan)
+values ("Cliente", true, "clientePlanPremium", "clientepremium@gmail.com", "Roberto", "cliente123", "Rol de cliente", "PREMIUM");
